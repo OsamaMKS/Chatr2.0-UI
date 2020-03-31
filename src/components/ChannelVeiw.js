8f4bdeb9a5160d293f1a356e7f1b229a6878b844
Osama Sider <osamamksider@hotmail.com>
2020-03-31 16:56:56 +0300

    fixed the empty channels

--- src/components/ChannelVeiw.js
+++ src/components/ChannelVeiw.js
@@ -4,69 +4,58 @@ import { connect } from "react-redux";
 import { fetchChannels, channelDetail } from "../redux/actions";
 
 class ChannelVeiw extends Component {
-    componentDidMount() {
+  componentDidMount() {
+    this.props.ChannelDetail(this.props.match.params.channelID);
+  }
+  getMsg = () => {
+    return this.props.channel.find(msg => {
+      if (msg.channel) {
+        return msg;
+      }
+    });
+  };
 
+  componentDidUpdate(prevProps) {
+    const channelID = this.props.match.params.channelID;
+    if (prevProps.match.params.channelID !== channelID) {
+      this.props.ChannelDetail(channelID);
     }
-    getMsg = () => {
-        return (
-            this.props.channel.find(msg => {
-                if (msg.channel) {
-                    return msg
-                }
-            }
-            ))
+  }
+
+  render() {
+    if (this.props.channel) {
+      if (
+        this.props.channel.filter(
+          channel => channel.channel === this.props.channels.id
+        )
+      ) {
+        return this.props.channel.map(msg => {
+          return (
+            <div style={{ marginLeft: "20px" }}>
+              {msg.message}
+              <br></br>
+            </div>
+          );
+        });
+      } else {
+        return <div>Loading...</div>;
+      }
+    } else {
+      return <div>Loading...</div>;
     }
-    componentWillUpdate() {
-
-
-        if (this.props.channel !== undefined) {
-            if (this.props.channel === null)
-                this.props.ChannelDetail(this.props.match.params.channelID);
-
-            else if (this.props.channel && (+this.props.match.params.channelID !== +this.getMsg().channel)) {
-                this.props.ChannelDetail(this.props.match.params.channelID);
-                console.log(this.getMsg())
-            }
-
-        }
-    }
-
-
-
-
-    render() {
-        if (this.props.channel) {
-            if (this.props.channel.filter(channel => channel.channel === this.props.channels.id)) {
-                return (
-                    this.props.channel.map(msg => {
-                        return (
-                            <div style={{ marginLeft: "20px" }}>
-                                {msg.message}
-                                <br></br>
-                            </div>
-
-                        )
-                    }))
-            }
-            else {
-                return <div>Loading...</div>;
-            }
-        } else {
-            return <div>Loading...</div>;
-        }
-    }
+  }
 }
 const mapStateToProps = state => {
-    return {
-        user: state.user,
-        channels: state.rootChannels.channels,
-        channel: state.rootChannel.channelDetail
-    };
+  return {
+    user: state.user,
+    channels: state.rootChannels.channels,
+    channel: state.rootChannel.channelDetail
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        ChannelDetail: channelID => dispatch(channelDetail(channelID))
-    };
+  return {
+    ChannelDetail: channelID => dispatch(channelDetail(channelID))
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(ChannelVeiw);
--- src/redux/actions/channel.js
+++ src/redux/actions/channel.js
@@ -3,7 +3,7 @@ import { setErrors } from "./errors";
 import instance from "./instance";
 
 export const channelDetail = channelID => async dispatch => {
-  dispatch({ type: CHANNEL_DETAIL });
+  dispatch({ type: CHANNEL_DETAIL, payload: null });
   try {
     const res = await instance.get(`channels/${channelID}/`);
     const channel = res.data;
